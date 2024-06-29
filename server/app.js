35dd58c55898fa58f9f341dad510cc0ea0ab93d9
KavanBakori <kavanbakori111@gmail.com>
2024-06-29 19:59:22 +0530

    data

--- server/app.js
+++ server/app.js
@@ -7,40 +7,48 @@ const { Userdetails, connectToDatabase } = require('./dbschemas');
 const app = express();
 const port = 3001;
 
-connectToDatabase(); 
+connectToDatabase();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/popupdata', async (req,res) => {
-    const { email, age,gender,weight,height,dietarypreferences,allergies,healthgoals } = req.body;
+app.post('/popupdata', async (req, res) => {
+  const { email, age, gender, weight, height, dietarypreferences, allergies, healthgoals } = req.body;
 
-    try {
-        const userdetails = new Userdetails({
-          email: email,
-          age: age,
-          gender: gender,
-          weight: weight,
-          height: height,
-          dietarypreferences: dietarypreferences,
-          allergies: allergies,
-          healthgoals: healthgoals
-        });
-    
-        const saveduserdetails = await userdetails.save();
-        console.log(saveduserdetails);
-        res.status(200).json(saveduserdetails);
-      } catch (error) {
-        console.error('Error creating suggestion:', error);
-        res.status(500).json({ error: 'An error occurred' });
-      }
+  try {
+    const userdetails = new Userdetails({
+      email: email,
+      age: age,
+      gender: gender,
+      weight: weight,
+      height: height,
+      dietarypreferences: dietarypreferences,
+      allergies: allergies,
+      healthgoals: healthgoals
     });
 
-    
+    const saveduserdetails = await userdetails.save();
+    console.log(saveduserdetails);
+    res.status(200).json(saveduserdetails);
+  } catch (error) {
+    console.error('Error creating suggestion:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.get('/fetchpopupdata', async (req, res) => {
+  try {
+    const pop = await Userdetails.find();
+    res.json(pop);
+  } catch (e) {
+    console.log(e);
+  }
+})
+
 app.get('/', (req, res) => {
-    res.send('Welcome to my server!');
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  res.send('Welcome to my server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
